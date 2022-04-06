036fe42c391df828f14b1deb5083d687335d4827
b08505045 <84371904+b08505045@users.noreply.github.com>
2022-04-06 16:54:34 +0800

    Create 2-4.c

---
+++ 2-4.c
@@ -0,0 +1,14 @@
+*temp = head, *ans = head
+K = 1, target = 0 // target = the number of floor(2K/5)
+while(temp != NULL)
+    temp = temp -> next
+    K++
+    // if target != floor(2K/5), update target and ans
+    if(target != floor(2*K / 5)
+        target = floor(2*K / 5)
+        // since floor(2K/5) is sequentially 2, 4, 6, 8, 10..., and ans start from head (node 1)
+        // ans = ans -> next if ans is now node 1
+        if(target == 2)
+            ans = ans -> next
+        else
+            ans = ans -> next -> next
